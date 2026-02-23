9719c30e696408f3f9992ef1d013680e5288e104
lucky rana <luckyranaluckyrana995@gmail.com>
2026-02-23 10:00:58 +0530

    Add files via upload

---
+++ 23.c
@@ -0,0 +1,97 @@
+/*Merge Two Sorted Linked Lists - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers (first list)
+- Third line: integer m
+- Fourth line: m space-separated integers (second list)
+
+Output:
+- Print the merged linked list elements, space-separated*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+
+struct Node* insertEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+
+struct Node* mergeLists(struct Node* l1, struct Node* l2) {
+    struct Node dummy;
+    struct Node* tail = &dummy;
+    dummy.next = NULL;
+
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->data <= l2->data) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+
+    if (l1 != NULL)
+        tail->next = l1;
+    else
+        tail->next = l2;
+
+    return dummy.next;
+}
+
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, m, i, value;
+    struct Node* list1 = NULL;
+    struct Node* list2 = NULL;
+    struct Node* merged = NULL;
+
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &value);
+        list1 = insertEnd(list1, value);
+    }
+
+    scanf("%d", &m);
+    for (i = 0; i < m; i++) {
+        scanf("%d", &value);
+        list2 = insertEnd(list2, value);
+    }
+
+    merged = mergeLists(list1, list2);
+
+    printList(merged);
+
+    return 0;
+}
