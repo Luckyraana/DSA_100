95a296346bbd0b1bc488edda95089ca0bba0fc42
lucky rana <luckyranaluckyrana995@gmail.com>
2026-03-01 11:23:56 +0530

    Add files via upload

---
+++ 29.c
@@ -0,0 +1,95 @@
+/*Rotate Linked List Right by k Places - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+- Third line: integer k
+
+Output:
+- Print the linked list elements after rotation, space-separated*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* rotateRight(struct Node* head, int k) {
+    if (head == NULL || k == 0)
+        return head;
+
+    struct Node* temp = head;
+    int n = 1;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+        n++;
+    }
+
+    k = k % n;
+    if (k == 0)
+        return head;
+
+   
+    temp->next = head;
+
+    int steps = n - k;
+    struct Node* newTail = head;
+    int i;  
+
+    for (i = 1; i < steps; i++) {
+        newTail = newTail->next;
+    }
+
+    struct Node* newHead = newTail->next;
+    newTail->next = NULL;
+
+    return newHead;
+}
+
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, k, value;
+    struct Node *head = NULL, *tail = NULL;
+    int i;   
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &value);
+        struct Node* newNode = createNode(value);
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    scanf("%d", &k);
+
+    head = rotateRight(head, k);
+
+    printList(head);
+
+    return 0;
+}
+
+
