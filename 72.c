abcbf2c041396c7b26ba986467e54068ac24ceb5
lucky rana <luckyranaluckyrana995@gmail.com>
2026-04-13 18:07:11 +0530

    Add files via upload

---
+++ 72.c
@@ -0,0 +1,30 @@
+/*Given a string s consisting of lowercase letters, find the first repeated character in the string. A character is considered repeated if it appears more than once, and among all such characters, the one whose second occurrence has the smallest index should be returned.
+
+Input Format
+A single string s.
+
+Output Format
+Print the first repeated character. If no character is repeated, print -1.*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    scanf("%s", s);
+
+    int freq[26] = {0} , i;
+
+    for ( i = 0; s[i] != '\0'; i++) {
+        freq[s[i] - 'a']++;
+        if (freq[s[i] - 'a'] == 2) {
+            printf("%c", s[i]);
+            return 0;
+        }
+    }
+
+    printf("-1");
+    return 0;
+}
+
+
